9cfef3c851bda60d681b4ab5da8f960f433c971d
Sahan Kaushalya <mkskaushalya@gmail.com>
2024-04-21 19:27:10 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,3 +14,12 @@ backToTop.addEventListener('click', function() {
     window.scrollTo({ top: 0, behavior: 'hidden' });
 });
 
+// Hamberger Menu
+var hamburger = document.querySelector('#hamburger');
+var nav = document.querySelector('nav');
+
+hamburger.addEventListener('click', function() {
+    nav.classList.toggle('active');
+});
+
+
