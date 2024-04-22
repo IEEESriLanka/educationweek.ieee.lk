74b65b7804399528a0cd6714f3adac26033e37fd
Sahan Kaushalya <mkskaushalya@gmail.com>
2024-04-22 16:56:41 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -26,3 +26,22 @@ $(function() {
     $('img.lazy').lazyload();
 });
 
+var btnday1 = document.getElementById('btnday1');
+var btnday2 = document.getElementById('btnday2');
+
+var day1cont = document.getElementById('day1cont');
+var day2cont = document.getElementById('day2cont');
+
+btnday1.addEventListener('click', function() {
+    day1cont.classList.add('active');
+    day2cont.classList.remove('active');
+    btnday1.classList.add('active');
+    btnday2.classList.remove('active');
+});
+
+btnday2.addEventListener('click', function() {
+    day2cont.classList.add('active');
+    day1cont.classList.remove('active');
+    btnday2.classList.add('active');
+    btnday1.classList.remove('active');
+});
