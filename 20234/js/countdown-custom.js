44a900772e3a0ce264a423e47d50cbc2cf150b23
Sahan Kaushalya <mkskaushalya@gmail.com>
2024-04-23 14:31:58 +0530

    jj

---
+++ 20234/js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2023, 03, 03, 8)}); // year, month, date, hour
+        });
+});		
+
